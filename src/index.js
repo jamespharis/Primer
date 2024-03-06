24270635e03102fb6fba6e0c752868db3a0856d9
James Pharis <jamesmpharis@gmail.com>
2024-03-06 14:30:13 -0500

    Completed Primer Assignment.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 export function calculateTotal (items, tax) {
-  // TODO
+  let total = 0
+  tax = Math.abs(tax)
+  for (let item of items) { // iterate through objects in the array
+    total += item.price // add the price of the item
+    if (item.taxable) { total += item.price*tax } //add the tax if taxable
+  }
+  return total // return a number representing the total cost of all the items including the tax on items that can be taxed
 }
